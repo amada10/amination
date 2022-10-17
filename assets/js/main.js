1ab404c093f23dce36ccbeafa7dc3f795212640b
diamondrarktvo <amada.rktvo@gmail.com>
2022-10-17 22:51:21 +0300

    restore main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,17 @@
-let body = document.body;
-let moveBackgroundMouse = document.querySelector('.background_mouse');
-body.addEventListener('mousemove', (event) => {
-    let x = event.pageX - 40;
-    let y = event.pageY - 40;
-    moveBackgroundMouse.style.top = y + 'px';
-    moveBackgroundMouse.style.left = x + 'px';
+let card = document.querySelectorAll('.card');
+
+card.forEach((oneCard) => {
+    oneCard.addEventListener('mouseover', () => {
+        oneCard.childNodes[3].style.display = 'flex';
+    });
+    oneCard.addEventListener('mouseout', () => {
+        oneCard.childNodes[3].style.display = 'none';
+    });
 })
+
+let year = document.getElementById('anneeCopyright');
+let date = new Date();
+year.textContent = date.getFullYear();
+
+let srcVideoElement = document.getElementById('sourceVideo');
+let topTab = document.querySelectorAll('.top-tab-item');
