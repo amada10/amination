babddaf4989847fb567858f8796033f5c6fc0447
diamondrarktvo <amada.rktvo@gmail.com>
2022-10-17 23:38:49 +0300

    test changement video background dynamic

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,4 +14,15 @@ let date = new Date();
 year.textContent = date.getFullYear();
 
 let srcVideoElement = document.getElementById('sourceVideo');
+let video = document.querySelector('.myVideoBg');
 let topTab = document.querySelectorAll('.top-tab-item');
+
+topTab.forEach((topItem) => {
+    topItem.addEventListener('click', function(){
+        if(topItem.textContent === "Design"){
+            //srcVideoElement.src="assets/video/design.mp4";
+            srcVideoElement.setAttribute('src', 'assets/video/design.mp4'),
+            console.log(srcVideoElement);
+        }
+    })
+})
